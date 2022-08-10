46fd239212dbc68b54f3c73a690699594b8f78e8
Eng. Umair Asad <77408034+umair-asad-bhatti@users.noreply.github.com>
2022-08-10 07:00:48 +0000

    Update Channel.jsx

--- src/Pages/Channel.jsx
+++ src/Pages/Channel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Channel = () => {
     return (
-        <div className='w-[100vw]'>Channel</div>
+        <div className='w-[100vw]'>Channel page has to be build yet</div>
     )
 }
 
