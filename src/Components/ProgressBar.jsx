07de7380219fcab1f8947b7043042e15afc4e0a3
Eng. Umair Asad <77408034+umair-asad-bhatti@users.noreply.github.com>
2022-08-14 01:43:28 +0000

    fixed progress bar

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -3,23 +3,17 @@ import greenBar from '../assets/greenBar.svg'
 
 const ProgressBar = ({ mainHeading, perHeading, instruction }) => {
     return (
-        <div className='flex items-center flex-wrap justify-between gap-4 overflow-hidden  py-4'>
-
-
+        <div className='flex md:items-center items-start flex-wrap md:flex-row flex-col justify-between gap-4 overflow-hidden  py-4'>
             <h1 className='dark:text-white text-blue-darkmdbg font-semibold text-xl'>{mainHeading}</h1>
-
-
             <h1 className='text-green-600 font-bold text-lg'>{perHeading}</h1>
             <img src={greenBar} alt="" />
-            <div className='w-6 h-6 rounded-full bg-green-800 p-1'>
+            <div className=' md:block hidden w-6 h-6 rounded-full bg-green-800 p-1'>
                 <div className='bg-green-500 w-full h-full rounded-full animate-fade'>
 
 
                 </div>
-
             </div>
-            <h1 className='text-green-600 font-bold text-lg md:block'>{instruction}</h1>
-
+            <h1 className='text-green-600 font-bold text-lg md:block hidden'>{instruction}</h1>
         </div>
     )
 }
