5bf8153a2e34153812006e0b89c120c47a92727c
Eng. Umair Asad <77408034+umair-asad-bhatti@users.noreply.github.com>
2022-08-14 02:12:18 +0000

    Progress Bar done

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -4,16 +4,16 @@ import './style.css'
 
 const ProgressBar = ({ mainHeading, perHeading, instruction }) => {
     return (
-        <div className='wrapper flex md:items-center items-start  md:flex-row flex-col justify-between gap-4 overflow-hidden  py-4'>
-            <h1 className='dark:text-white w-[100px]  text-blue-darkmdbg font-semibold text-xl'>{mainHeading}</h1>
-            <h1 className='text-green-600 font-bold text-lg'>{perHeading}</h1>
-            <img src={greenBar} alt="" />
-            <div className='blink w-6 h-6 rounded-full bg-green-800 p-1'>
+        <div className='wrapper flex md:items-center items-start  md:flex-row flex-col justify-between  overflow-hidden '>
+            <h1 className=' dark:text-white heading w-[150px]   p-2  text-blue-darkmdbg font-semibold text-xl'>{mainHeading}</h1>
+            <h1 className='text-green-600   w-[100px]  p-2 m-0 font-bold text-lg'>{perHeading}</h1>
+            <img src={greenBar} className='image m-0 p-2' alt="bar" />
+            <div className='blink w-5 h-5 rounded-full bg-green-800 p-1'>
                 <div className='bg-green-500 w-full h-full rounded-full animate-fade'>
 
                 </div>
             </div>
-            <h1 className='text-green-600 font-bold text-lg md:block hidden instruction'>{instruction}</h1>
+            <h1 className='text-green-600 font-bold text-normal md:block hidden instruction'>{instruction}</h1>
         </div>
     )
 }
